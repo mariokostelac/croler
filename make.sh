21a461cb26438a68d9e21b056fdbcfaf9e680080
Mario Kostelac <mario.kostelac@gmail.com>
2014-11-25 11:30:24 +0000

    added building consensus phase

--- make.sh
+++ make.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 set -ex
+
+# build overlap phase
 cd pipeline/qpid
 make
+
+# build layout phase
 cd ../brahle_assembly
+if [[ ! -d bin ]]; then
+    mkdir bin
+fi
+make
+
+# build consensus phase
+cd ../msa
 if [[ ! -d bin ]]; then
     mkdir bin
 fi
