794fa3c8715cb88b045740184182adb4d3721081
Mario Kostelac <mario.kostelac@gmail.com>
2014-12-27 18:08:44 +0000

    fixed run script (issue #20)

--- run.sh
+++ run.sh
@@ -1,13 +1,30 @@
 #!/usr/bin/env bash
 
 if [[ $# -ne 1 ]]; then
-    echo "usage: $0 <reads_file.fasta>"
+    echo "usage: $0 <reads_file.fastq>"
     exit 1
 fi
+
+line="---------------------------------------------------------------"
 
 TMP_OVERLAPS=overlaps.afg
 TMP_LAYOUT=layout.afg
 
-./pipeline/qpid/bin/overlap $1 -o $TMP_OVERLAPS
-./pipeline/brahle_assembly/bin/main_layout $1 $TMP_OVERLAPS
-grbin-msa $1 $TMP_LAYOUT
+set -e
+
+echo "OVERLAP phase"
+echo $line
+./bin/overlap -f $1 -O $TMP_OVERLAPS
+echo $line
+echo
+
+echo "LAYOUT phase"
+echo $line
+./bin/layout -f $1 $TMP_OVERLAPS
+echo $line
+echo
+
+echo "CONSENSUS phase"
+echo $line
+./bin/consensus $1 $TMP_LAYOUT
+echo $line
