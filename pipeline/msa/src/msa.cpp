12594ec81907b8e508833f58bba90b9282d45c53
Mario Kostelac <mario.kostelac@gmail.com>
2015-01-04 17:41:26 +0000

    consensus: added support for reading .afg files (#40)

--- pipeline/msa/src/msa.cpp
+++ pipeline/msa/src/msa.cpp
@@ -2,6 +2,10 @@
 #include "FastaFile.h"
 #include "LayoutFile.h"
 
+#include "lib/amos/reader.cpp"
+
+#include <cassert>
+#include <unordered_map>
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -14,7 +18,7 @@ using namespace std;
 
 void usage(char *path) {
   printf(
-    "usage: %s [-p] [-e EPSILON] [-m MAX_RADIUS] reads-fasta layout-afg\n"
+    "usage: %s [-p] [-e EPSILON] [-m MAX_RADIUS] <reads-afg> <layout-afg>\n"
     "\t-p is no-pack, disable packing non-interscted reads to same rows\n"
     "\t-e (=0.001) EPSILON, "
       "band size in edit-distance as percent of maximum offset\n"
@@ -46,17 +50,16 @@ int doit(
     float epsilon,
     int max_radius,
     bool pack,
-    const char *fasta_file,
+    const char *afg_file,
     const char *layout_file,
     const char *cons_file = "consensus.fasta") {
-  FastaFile fasta(fasta_file);
+
   FastaFile output(cons_file);
   LayoutFile layout(layout_file);
-  // erase contents of cons_file
-  fclose(fopen(cons_file, "w"));
 
-  if (!fasta.initialize()) {
-    fprintf(stderr, "%s", fasta.getError().c_str());
+  vector<AMOS::Read*> reads;
+  if (AMOS::get_reads(reads, afg_file) < 0) {
+    fprintf(stderr, "Error while reading '%s'\n", afg_file);
     return 1;
   }
 
@@ -65,20 +68,32 @@ int doit(
     return 1;
   }
 
+  // erase contents of cons_file
+  fclose(fopen(cons_file, "w"));
+
+  // map reads so we can get them by real ids
+  unordered_map<uint32_t, AMOS::Read*> read_by_id;
+  int reads_size = reads.size();
+  for (int i = 0; i < reads_size; ++i) {
+    const auto& read = reads[i];
+    assert(!read_by_id.count(read->iid));
+    read_by_id[read->iid] = read;
+  }
+
   auto contigs = layout.getContigs();
   for (const auto& contig : contigs) {
     printf("processing new contig with %ld reads..\n", contig.size());
     MultipleAligner MA(epsilon, max_radius, pack);
-    for (auto read : contig) {
-      auto read_str = fasta.getRead(read.read_id);
-      int size = read.hi - read.lo; // can be neg!
-      if (read.hi < read.lo) {
-        swap(read.lo, read.hi);
+    for (auto c_part : contig) {
+      AMOS::Read* read = read_by_id[c_part.read_id];
+      int size = c_part.hi - c_part.lo; // can be neg!
+      if (c_part.hi < c_part.lo) {
+        swap(c_part.lo, c_part.hi);
       }
       MA.addSequence(
-          read_str.first + read.lo,
+          read->seq + c_part.lo,
           size,
-          read.offset);
+          c_part.offset);
     }
     output.appendRead(MA.getConsensus(NULL));
   }
